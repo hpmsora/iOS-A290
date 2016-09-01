38428852623284e9c06c6ce96ae75d57afd11d81
Won Yong Ha <Owl@140-182-196-186.dhcp-bl.indiana.edu>
2016-08-31 20:04:18 -0400

    31 Aug 2016

--- Lab104/FlashCards/FlashCards/ViewController.swift
+++ Lab104/FlashCards/FlashCards/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func showQuestion(sender: UIButton) {
+        self.questionLabel.text = "How old are you?"
+        self.answerLabel.text = "(try guessing...)"
+    }
+
+    @IBAction func showAnswer(sender: UIButton) {
+        self.answerLabel.text = "I'm 10 years old."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
